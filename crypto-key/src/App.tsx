c87735164f0f6676e2bc46fb4239d6bb577f1f88
Marin Grabovac <grabovacmarin000@gmail.com>
2025-10-17 21:48:25 +0200

    Redesigned snow

--- crypto-key/src/App.tsx
+++ crypto-key/src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { listen } from "@tauri-apps/api/event";
-import { Snowfall } from "./components/shared/Snowfall";
 import { LoggerView } from "./components/views/react/LoggerView";
 import { AppView } from "./components/views/react/AppView";
+import Snowfall from 'react-snowfall';
+import { SnowfallCustom } from "./components/shared/Snowfall";
 
 const App: React.FC = () => {
   const [activeView, setActiveView] = useState<"app" | "logger">("app");
@@ -18,10 +19,24 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <>
-      <Snowfall />
-      {activeView === "app" ? <div className="app-container"><AppView /></div> : <div className="logger-container"><LoggerView /></div>}
-    </>
+    <div style={{ position: 'relative', width: '100%' }}>
+      <Snowfall
+        snowflakeCount={300}
+        color="#61a0ec"
+        style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', pointerEvents: 'none', zIndex: 1000000 }}
+      />
+      <SnowfallCustom />
+
+      {activeView === "app" ? (
+        <div className="app-container" style={{ position: 'relative', zIndex: 10 }}>
+          <AppView />
+        </div>
+      ) : (
+        <div className="logger-container" style={{ position: 'relative', zIndex: 10 }}>
+          <LoggerView />
+        </div>
+      )}
+    </div>
   );
 };
 
--- crypto-key/src/components/shared/Snowfall.tsx
+++ crypto-key/src/components/shared/Snowfall.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-export const Snowfall: React.FC = () => {
+export const SnowfallCustom: React.FC = () => {
   const [flakes, setFlakes] = useState<React.ReactElement[]>([]);
 
   useEffect(() => {
